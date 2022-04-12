1ea805ca2ed818949e2a4b7bbfcea0ed62a50543
mxinburritos <mxinburritos@gmail.com>
2022-04-12 13:54:36 -0400

    Fix mobile styles

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -4,27 +4,32 @@ import { QRCodeSVG } from "qrcode.react";
 
 const Widget = ({ title }: { title: string }) => (
   <Box boxShadow="lg" padding="24px 32px">
-    <Heading fontWeight={500} marginBottom="10px" fontSize="24px" color="#212121">
+    <Heading fontWeight={500} marginBottom="10px" size="lg" color="#212121">
       {title}
     </Heading>
-    <Text color="#858585" fontSize="16px" fontWeight={400}>
+    <Text color="#858585" fontSize="sm" fontWeight={400}>
       Short Description of Title
     </Text>
   </Box>
 );
 
 const Admin = () => (
-  <Flex flexDir="column" padding="48px 72px" gap="20px">
+  <Flex flexDir="column" padding={{ base: "0", md: "48px 72px" }} gap="20px">
     <Flex
-      flexDir="row"
-      bgGradient="linear(to-r, #33c2ff, #7b69ec)"
+      flexDir={{ base: "column", md: "row" }}
+      bgGradient={{ base: "linear(to-b, #33c2ff, #7b69ec)", md: "linear(to-r, #33c2ff, #7b69ec)" }}
       boxShadow="lg"
       alignItems="center"
       justifyContent="space-around"
     >
-      <Box color="white" paddingY="64px" paddingLeft="64px">
-        <Heading fontSize="64px">Welcome Back Noah!</Heading>
-        <Text fontSize="38px">
+      <Box
+        color="white"
+        paddingY={{ base: "32px", md: "64px" }}
+        paddingLeft={{ base: "16px", md: "64px" }}
+        paddingRight={{ base: "16px", md: "0" }}
+      >
+        <Heading size="3xl">Welcome Back Noah!</Heading>
+        <Text fontSize="lg">
           We're happy to see that you're an{" "}
           <Text as="span" color="#4569e9">
             attending staff member.
@@ -39,8 +44,9 @@ const Admin = () => (
         borderRadius="3xl"
         padding="10px"
         bgColor="#b4c0fa"
-        margin="40px"
-        marginRight="94px"
+        marginY={{ md: "40px" }}
+        marginBottom={{ base: "40px" }}
+        marginX={{ base: "auto", md: "64px" }}
       >
         <QRCodeSVG
           value={JSON.stringify({

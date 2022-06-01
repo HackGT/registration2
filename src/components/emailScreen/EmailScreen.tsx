65704f91a63d270fb0eef7994e11d28550aec589
Aayush Seth <aayushseth8@gmail.com>
2022-06-01 00:33:56 -0700

    Add intial components (pre-styling)

--- src/components/emailScreen/EmailScreen.tsx
+++ src/components/emailScreen/EmailScreen.tsx
@@ -1,16 +1,67 @@
 import React from "react";
-import { Box, Heading, Text, Button, Tabs, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Text,
+  Textarea,
+  Button,
+  Tabs,
+  Input,
+  Select,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
 
 const EmailScreen: React.FC = () => (
-  <Box
-    color="Black"
-    paddingY={{ base: "32px", md: "32px" }}
-    paddingLeft={{ base: "16px", md: "64px" }}
-    paddingRight={{ base: "16px", md: "0" }}
-  >
-    <Heading size="2xl">Email Screen</Heading>
-    <Text fontSize="lg">test text</Text>
-  </Box>
+  <>
+    <Box
+      color="Black"
+      paddingY={{ base: "32px", md: "32px" }}
+      paddingLeft={{ base: "16px", md: "64px" }}
+      paddingRight={{ base: "16px", md: "0" }}
+    >
+      <Heading size="2xl">Send a Batch Email</Heading>
+      <Text fontSize="lg">test text</Text>
+    </Box>
+
+    {/* left side */}
+    <Box>
+      <Select placeholder="Select Recipient">
+        <option value="participants">Participants</option>
+        <option value="sponsors">Sponsors</option>
+        <option value="mentors">Mentors</option>
+      </Select>
+
+      <Input placeholder="Subject" />
+
+      <Textarea placeholder="Email Content..." />
+      <Button colorScheme="purple">Send</Button>
+    </Box>
+
+    {/* right side */}
+    <Box>
+      <Tabs variant="enclosed">
+        <TabList>
+          <Tab>Format Guide</Tab>
+          <Tab>Email Log</Tab>
+          <Tab>Preview</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <p>format guide...</p>
+          </TabPanel>
+          <TabPanel>
+            <p>email log...</p>
+          </TabPanel>
+          <TabPanel>
+            <p>preview...</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
+  </>
 );
 
 export default EmailScreen;

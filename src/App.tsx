02e6f933c8126316e9e26df43482986fed404e21
Ayush Goyal <agoyal2001@gmail.com>
2022-06-14 18:04:36 -0700

    Rename admin to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { useLogin } from "./hooks/useLogin";
 import FormPlayground from "./components/formPlayground/FormPlayground";
-import Admin from "./components/dashboard/Dashboard";
+import Dashboard from "./components/dashboard/Dashboard";
 import User from "./components/user/User";
 import Loading from "./util/Loading";
 import EmailScreen from "./components/emailScreen/EmailScreen";
@@ -24,21 +24,19 @@ export const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
-        {
-          (loading || !loggedIn) ? (
-            <Loading />
-          ) : (
-            <Routes>
-              <Route path="/" element={<SelectEvent />} />
-              <Route path="/:hexathonId">
-                <Route path="" element={<Admin />} />
-                <Route path="user/:userId" element={<User />} />
-                <Route path="form-playground" element={<FormPlayground />} />
-                <Route path="email" element={<EmailScreen />} />
-              </Route>
-            </Routes>
-          )
-        }
+        {loading || !loggedIn ? (
+          <Loading />
+        ) : (
+          <Routes>
+            <Route path="/" element={<SelectEvent />} />
+            <Route path="/:hexathonId">
+              <Route path="" element={<Dashboard />} />
+              <Route path="user/:userId" element={<User />} />
+              <Route path="form-playground" element={<FormPlayground />} />
+              <Route path="email" element={<EmailScreen />} />
+            </Route>
+          </Routes>
+        )}
       </AuthProvider>
     </ChakraProvider>
   );

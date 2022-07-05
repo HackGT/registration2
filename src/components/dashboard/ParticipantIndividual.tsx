b751a0dbf9fdac276187faf04913029b3d79f20c
Adityaa Magesh Kumar <adityaa.magesh@gmail.com>
2022-07-05 19:23:52 -0400

    fix axios api call

--- src/components/dashboard/ParticipantIndividual.tsx
+++ src/components/dashboard/ParticipantIndividual.tsx
@@ -1,72 +1,38 @@
-import React, { useState, useEffect } from "react";
-import { Box, Flex, SimpleGrid, Heading, Text, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Tag, TagLabel, TagLeftIcon, TagRightIcon, TagCloseButton } from "@chakra-ui/react";
-import axios from "axios";
+import React from "react";
+import Loading from "../../util/Loading";
+import { Box, Heading, Text, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, Tag } from "@chakra-ui/react";
+import useAxios from "axios-hooks";
 import { useParams } from "react-router-dom";
 
 const ParticipantIndividual: React.FC = () => {
   const { applicationId } = useParams();
-  const [name, setName] = useState("name");
-  const [gmail, setGmail] = useState("gmail");
-  const [phone, setPhone] = useState("phone");
-  const [linkedin, setLinkedin] = useState("linkedin");
-  const [adult, setAdult] = useState(true);
-  const [school, setSchool] = useState("school");
-  const [major, setMajor] = useState("major");
-  const [graduation, setGraduation] = useState(0);
-  const [gender, setGender] = useState("gender");
-  const [ethnicity, setEthnicity] = useState("ethnicity");
-  const [applied, setApplied] = useState(false);
-  const [confirmed, setConfirmed] = useState(false);
+  const [{ data, loading, error }] = useAxios(`https://registration.api.hexlabs.org/application/${applicationId}`);
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const app = await axios.get(
-          `https://registration.api.hexlabs.org/application/${applicationId}`
-        );
-        const user = await axios.get(
-          `https://users.api.hexlabs.org/users/'${app.data.applicationData.userId}`
-        )
-        setName(`${user.data.name.first}{' '}${user.data.name.last}`);
-        setGmail(user.data.email);
-        setPhone(app.data.applicationData.phoneNumber);
-        setLinkedin(app.data.applicationData.linkedin);
-        setAdult(app.data.applicationData.adult);
-        setSchool(app.data.applicationData.school);
-        setMajor(app.data.applicationData.major);
-        setGraduation(app.data.applicationData.graduationYear);
-        setGender(app.data.applicationData.gender);
-        setEthnicity(app.data.applicationData.ethnicity);
-        setApplied(app.data.applied);
-        setConfirmed(app.data.confirmed);
-      } catch (e: any) {
-        console.log(e.message);
-      }
-    };
-    getUserData();
-  }, []);
+  if (loading || error) {
+    return <Loading />;
+  }
 
   return (
     <Box>
       <Box style={{paddingLeft: '50px'}}>
-        <Heading as="h1" size="xl" fontWeight={700} style={{paddingTop: '30px'}}> {name} </Heading>
-        <Heading as="h2" size="xs" fontWeight={500} style={{color: 'grey'}}> Hackathon Track </Heading>
-        <Text><Tag colorScheme='blue'>{applied ? 'Applied' : 'Not Applied'}</Tag> <Tag colorScheme='green'>{confirmed ? 'Confirmed' : 'Not Confirmed'}</Tag></Text>
+        <Heading as="h1" size="xl" fontWeight={700} style={{paddingTop: '30px'}}>{data.name.first} {data.name.last}</Heading>
+        <Heading as="h2" size="xs" fontWeight={500} style={{color: 'grey'}}>Hackathon Track: {data.applicationBranch.name}</Heading>
+        <Text><Tag colorScheme='blue'>{data.applied ? 'Applied' : 'Not Applied'}</Tag> <Tag colorScheme='green'>{data.confirmed ? 'Confirmed' : 'Not Confirmed'}</Tag></Text>
       </Box>
       <Accordion style={{paddingTop: '10px', paddingLeft: '50px', paddingRight: '50px'}} defaultIndex={[0]} allowMultiple>
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <Box flex='1' textAlign='left' /* style={{ borderStyle: "solid", borderColor: "black", borderWidth: 1}} */>
+              <Box flex='1' textAlign='left'>
                 <Text style={{fontWeight: "bold"}}>Contact Information</Text>
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h2>
           <AccordionPanel pb={3}>
-            <Text>{gmail}</Text>
-            <Text>{phone}</Text>
-            <Text>{linkedin}</Text>
+            <Text>{data.email}</Text>
+            <Text>{data.phoneNumber}</Text>
+            <Text>{data.applicationData.linkedin}</Text>
           </AccordionPanel>
         </AccordionItem>
 
@@ -81,31 +47,31 @@ const ParticipantIndividual: React.FC = () => {
           </h2>
           <AccordionPanel pb={4}>
             <Text>
-              {adult ? 'Older' : 'Younger'} than 18 years old
+              {data.applicationData.adult ? 'Older' : 'Younger'} than 18 years old
             </Text>
             <Text>
               <span style={{color: "Grey"}}>
                 Attends
               </span> 
-              {" "} {school}
+              {" "} {data.applicationData.school}
             </Text>
             <Text>
               <span style={{color: "Grey"}}>
                 Studies
               </span> 
-              {" "} {major}
+              {" "} {data.applicationData.major}
             </Text>
             <Text>
               <span style={{color: "Grey"}}>
                 Identifies
               </span> 
-              {" "} as a {gender}
+              {" "} as a {data.applicationData.gender}
             </Text>
             <Text>
               <span style={{color: "Grey"}}>
                 Ethnicity
               </span> 
-              {" "} is {ethnicity}
+              {" "} is {data.applicationDataa.ethnicity}
             </Text>
           </AccordionPanel>
         </AccordionItem>

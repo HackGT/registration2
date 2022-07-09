b5fe2070281dd916b58cba59d4806dd2e4176c59
Aayush Seth <aayushseth8@gmail.com>
2022-07-09 17:17:39 -0400

    Add styles and links

--- src/components/selectEvent/EventCard.tsx
+++ src/components/selectEvent/EventCard.tsx
@@ -1,17 +1,38 @@
 import React from "react";
 
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, LinkBox, LinkOverlay } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 interface Props {
   name: string;
+  id: string;
   description?: string;
   image?: string;
 }
 
 const EventCard: React.FC<Props> = props => (
-  <Box bg="tomato" w="100%" p={4} color="white">
-    {props.name}
-  </Box>
+  <LinkBox
+    bg="white"
+    w="90%"
+    p={4}
+    color="black"
+    border="1px"
+    borderColor="gray.200"
+    borderRadius="md"
+    shadow="sm"
+    h="150px"
+    transition=".5s"
+    marginBottom="10px"
+    _hover={{
+      shadow: "md",
+    }}
+  >
+    <Box>
+      <LinkOverlay href={`/${props.id}`}>
+        <Heading>{props.name}</Heading>
+      </LinkOverlay>
+    </Box>
+  </LinkBox>
 );
 
 export default EventCard;
--- src/components/selectEvent/SelectEvent.tsx
+++ src/components/selectEvent/SelectEvent.tsx
@@ -1,18 +1,19 @@
+import { VStack } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { useHexathons } from "../../contexts/HexathonsContext";
 import EventCard from "./EventCard";
 
 const SelectEvent: React.FC = () => {
   const { hexathons } = useHexathons();
-  console.log(hexathons);
+
   return (
-    <>
-      <h2>Select an event</h2>
+    <VStack paddingY={{ base: "32px", md: "32px" }}>
       {hexathons.map((hexathon: any) => (
-        <EventCard name={hexathon.name} />
+        <EventCard name={hexathon.name} id={hexathon._id} />
       ))}
-    </>
+    </VStack>
   );
 };
 

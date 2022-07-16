03541b09a4254da182b6f237927e2b99125e1de0
mshen63 <mshen63@gatech.edu>
2022-07-15 18:41:24 -0700

    Add suggested changes

--- src/components/dashboard/Tile.tsx
+++ src/components/dashboard/Tile.tsx
@@ -16,11 +16,11 @@ import {
   useDisclosure,
   Button,
 } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import ApplicationStatus from "../../types/ApplicationStatus";
 
 interface Props {
-  title: string;
-  description: string;
-  branchId: string;
+  branch: any;
   image?: string;
   status: string;
   hasApplication: boolean;
@@ -30,27 +30,53 @@ interface Props {
 
 
 const Tile: React.FC<Props> = props => {
+  const navigate = useNavigate();
   const { isOpen, onOpen, onClose } = useDisclosure()
   const cancelRef = React.useRef(null)
 
   const onConfirm = async () => {
     onClose()
     try {
-      const newAppLink = await props.chooseBranch(props.branchId)
-      window.open(newAppLink)
+      const newAppLink = await props.chooseBranch(props.branch._id)
+      if (newAppLink) {
+        navigate(newAppLink, { replace: true });
+      } else {
+        console.log("Undefined link form after choosing branch")
+      }
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <>
+    <LinkBox
+      borderRadius="4px"
+      boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+      onClick={async () => {
+        if (props.hasApplication) {
+          if (props.status == ApplicationStatus.NotStarted) {
+            onOpen()
+          } else if (props.status == ApplicationStatus.InProgress) {
+            navigate(props.currAppLink, { replace: true });
+          } else {
+            console.log("submitted!")
+          }
+        } else {
+          const newAppLink = await props.chooseBranch(props.branch._id)
+          if (newAppLink) {
+            navigate(newAppLink, { replace: true });
+          } else {
+            console.log("Issue getting new app link")
+          }
+        }
+      }}
+    >
       <AlertDialog
         isOpen={isOpen}
         leastDestructiveRef={cancelRef}
         onClose={onClose}
       >
-        <AlertDialogOverlay>
+        <AlertDialogOverlay >
           <AlertDialogContent>
             <AlertDialogHeader fontSize='lg' fontWeight='bold'>
               Start a new application?
@@ -71,30 +97,10 @@ const Tile: React.FC<Props> = props => {
           </AlertDialogContent>
         </AlertDialogOverlay>
       </AlertDialog>
-      <LinkBox
-      borderRadius="4px"
-      boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
-      onClick={async () => {
-        console.log("clicked!")
-        if (props.hasApplication) {
-          if (props.status=="Not Started") {
-            onOpen()
-          } else if (props.status=="In Progress") {
-            window.open(props.currAppLink)
-          } else {
-            console.log("submitted!")
-          }
-        } else {
-          const newAppLink = await props.chooseBranch(props.branchId)
-          window.open(newAppLink)
-        }
-      }}
-    >
       <Flex
         bgGradient={props.image ? "" : "linear(to-l, #33c2ff, #7b69ec)"}
         borderTopRadius="4px"
         height="150px"
-        onClick={() => console.log("Clicked!")}
         justifyContent="flex-end"
         alignItems="flex-start"
       >
@@ -106,16 +112,16 @@ const Tile: React.FC<Props> = props => {
       <Box padding="20px 32px">
         <Heading fontSize="18px" fontWeight="semibold" marginBottom="10px" color="#212121">
           <LinkOverlay href="#">
-            {props.title}
+            {props.branch.name}
           </LinkOverlay>
         </Heading>
         <Text fontSize="sm" color="#858585">
-          {props.description}
+          {`${new Date(props.branch.settings.open).toDateString()} - ${new Date(
+            props.branch.settings.close
+          ).toDateString()}`}
         </Text>
       </Box>
     </LinkBox>
-    </>
-    
   )
 };
 

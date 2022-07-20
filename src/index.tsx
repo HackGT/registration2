0d28a5e3027720f304be25a0e2a63f3b2f11b387
Ayush Goyal <agoyal2001@gmail.com>
2022-07-19 21:17:44 -0700

    Clean up and change deps

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import { App } from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,8 +14,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();

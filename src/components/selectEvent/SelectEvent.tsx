9fcb26d73e70b754eb87b199d28326b80e4a3c1c
Aayush Seth <aayushseth8@gmail.com>
2022-07-24 20:19:37 -0400

    Use Flex and Center on event select

--- src/components/selectEvent/SelectEvent.tsx
+++ src/components/selectEvent/SelectEvent.tsx
@@ -1,4 +1,4 @@
-import { VStack } from "@chakra-ui/react";
+import { Center, Flex } from "@chakra-ui/react";
 import React from "react";
 
 import EventCard from "./EventCard";
@@ -8,11 +8,13 @@ interface Props {
 }
 
 const SelectEvent: React.FC<Props> = props => (
-  <VStack paddingY={{ base: "32px", md: "32px" }}>
+  <Flex paddingY={{ base: "32px", md: "32px" }} direction="column" justify="center">
     {props.hexathons.map((hexathon: any) => (
-      <EventCard name={hexathon.name} id={hexathon._id} key={hexathon._id} />
+      <Center key={hexathon._id}>
+        <EventCard name={hexathon.name} id={hexathon._id} />
+      </Center>
     ))}
-  </VStack>
+  </Flex>
 );
 
 export default SelectEvent;

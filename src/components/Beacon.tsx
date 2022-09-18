e11d7a4093145da35534f42f4ffb9b697a0cd05b
Roshan Desai <rohod04@gmail.com>
2022-09-18 12:08:06 -0400

    Added beacon id as secret

--- src/components/Beacon.tsx
+++ src/components/Beacon.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 const HelpScoutBeacon: React.FC = () => (
-        <div>
-            <Helmet 
-            script={[{ 
-                type: 'text/javascript', 
-                innerHTML: '!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});'
-            }, {
-                type: 'text/javascript',
-                innerHTML: 'window.Beacon(\'init\', \'f459bd04-1c6f-4ed5-ae36-26a584971f73\')'
-            }]} 
-            />
-        </div>
-    )
+    <div>
+        <h1 hidden>{process.env.REACT_APP_HELPSCOUT_BEACON_ID}</h1>
+        <Helmet>
+            <script>
+            {`
+                !function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
+            `}
+            </script>
+            <script>
+                window.Beacon('init', document.getElementsByTagName('h1')[0].textContent);
+            </script>
+        </Helmet>
+    </div>
+)
+
 
 export default HelpScoutBeacon;

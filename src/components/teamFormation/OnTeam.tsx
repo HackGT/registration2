0839fe5c7a42cc1270bc2c43cbe1be15074aa1df
Yu Pan <70486009+yu-pan-cpu@users.noreply.github.com>
2022-09-25 00:40:59 -0400

    style on team page

--- src/components/teamFormation/OnTeam.tsx
+++ src/components/teamFormation/OnTeam.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Heading, Text, Input, Button } from "@chakra-ui/react";
+import { Box, Heading, Text, Input, Button, Center, VStack, HStack } from "@chakra-ui/react";
 import axios from "axios";
 import { apiUrl, Service } from "@hex-labs/core";
 
@@ -35,25 +35,36 @@ const OnTeam: React.FC<Props> = props => {
 
   return (
     <Box>
-      <Heading size="md" lineHeight="inherit">
-        Welcome to team {name}.
-      </Heading>
-      <Text>Current members: No one oh no</Text>
-      {props.team.members.length >= 4 && <Text>Y'all are at max capacity</Text>}
-
-      {props.team.members.length < 4 && (
-        <Box>
-          <Input
-            value={email}
-            onChange={changeEmail}
-            placeholder="Type in the email of someone you want to add"
-          />
-          <Button onClick={addMember}>Add team member</Button>
+      <VStack>
+        <Heading paddingTop="20px" paddingBottom="10px" size="lg" lineHeight="inherit">
+          Welcome to team {name}!
+        </Heading>
+        <Heading paddingTop="20px" paddingBottom="10px" size="sm" lineHeight="inherit">
+          Current members
+        </Heading>
+        <Box paddingBottom="100px">
+          {props.team.members.length >= 4 && <Text>Y'all are at max capacity</Text>}
+          {props.team.members.length < 4 && (
+            <Box>
+              <VStack>
+                <Heading paddingTop="20px" paddingBottom="10px" size="sm" lineHeight="inherit">
+                  Add more members to your team!
+                </Heading>
+                <HStack spacing="20px">
+                  <Input
+                    value={email}
+                    width="40vw"
+                    onChange={changeEmail}
+                    placeholder="Type in the email of someone you want to add"
+                  />
+                  <Button onClick={addMember}>Add</Button>
+                </HStack>
+              </VStack>
+            </Box>
+          )}
         </Box>
-      )}
-      <Box>
         <Button onClick={removeSelf}>Leave team</Button>
-      </Box>
+      </VStack>
     </Box>
   );
 };

d2415814e5de6a5bcd836ee0fac3a0b848cc4f96
Anupama1912 <anair325@gatech.edu>
2024-05-08 00:22:21 -0400

    corrected display when stats not found

--- src/components/admin/statistics/GraphAccordionSection.tsx
+++ src/components/admin/statistics/GraphAccordionSection.tsx
@@ -72,102 +72,94 @@ const GraphAccordionSection: React.FC<IProps> = props => {
       </h2>
       <AccordionPanel pb={4}>
         {isMobile ? <VStack>
-          {Object.keys(props.data.schoolData).length && (
+          {Object.keys(props.data.schoolData).length ? (
             <TableView heading="Universities" data={sortObjectByValue(props.data.schoolData)} />
-          )}
-          {Object.keys(props.data.majorData).length && (
+          ) : null}
+          {Object.keys(props.data.majorData).length ? (
             <TableView heading="Majors" data={sortObjectByValue(props.data.majorData)} />
-          )}
-          {Object.keys(props.data.schoolYearData).length && (
+          ) : null}
+          {Object.keys(props.data.schoolYearData).length ? (
             <BarGraphView
               heading="School Year"
               data={sortObjectByKey(props.data.schoolYearData)}
             />
-          )}
-          {Object.keys(props.data.shirtSizeData).length && (
+          ) : null}
+          {Object.keys(props.data.shirtSizeData).length ? (
             <TableView
               heading="Shirt Size"
               data={sortObjectByValue(props.data.shirtSizeData)}
             />
-          )}
-          {Object.keys(props.data.dietaryRestrictionsData).length && (
+          ) : null}
+          {Object.keys(props.data.dietaryRestrictionsData).length ? (
             <TableView
               heading="Dietary Restrictions"
               data={sortObjectByValue(props.data.dietaryRestrictionsData)}
             />
-          )}
-          {Object.keys(props.data.marketingData).length && (
-            <PieGraphView
-              heading="Marketing Source"
-              data={sortObjectByKey(props.data.marketingData)}
-            />
-          )}
-          {Object.keys(props.data.trackPreferenceData).length && (
+          ) : null}
+          {Object.keys(props.data.trackPreferenceData).length ? (
             <PieGraphView
               heading="Track Preference"
               data={sortObjectByKey(props.data.trackPreferenceData)}
             />
-          )}
-          {Object.keys(props.data.genderData).length && (
+          ) : null}
+          {Object.keys(props.data.marketingData).length ? (
+            <PieGraphView
+              heading="Marketing Source"
+              data={sortObjectByKey(props.data.marketingData)}
+            />
+          ) : null}
+          {Object.keys(props.data.genderData).length ? (
             <PieGraphView heading="Gender" data={sortObjectByKey(props.data.genderData)} />
-          )}
+          ) : null}
         </VStack>
         : <Grid templateColumns="repeat(3, 1fr)" templateRows="repeat(3, 1fr)" gap={7} overflowX="auto" mt={5} justifyItems="center">
-          <GridItem>
-            {Object.keys(props.data.schoolData).length && (
-              <TableView heading="Universities" data={sortObjectByValue(props.data.schoolData)} />
-            )}
-          </GridItem>
-          <GridItem>
-            {Object.keys(props.data.majorData).length && (
-              <TableView heading="Majors" data={sortObjectByValue(props.data.majorData)} />
-            )}
-          </GridItem>
-          <GridItem alignSelf="center">
-            {Object.keys(props.data.schoolYearData).length && (
-              <BarGraphView
-                heading="School Year"
-                data={sortObjectByKey(props.data.schoolYearData)}
-              />
-            )}
-          </GridItem>
-          <GridItem pt={10}>
-            {Object.keys(props.data.shirtSizeData).length && (
-              <TableView
-                heading="Shirt Size"
-                data={sortObjectByValue(props.data.shirtSizeData)}
-              />
-            )}
-          </GridItem>
-          <GridItem pt={10}>
-            {Object.keys(props.data.marketingData).length && (
-              <PieGraphView
-                heading="Marketing Source"
-                data={sortObjectByKey(props.data.marketingData)}
-              />
-            )}
-          </GridItem>
-          <GridItem rowSpan={2} pt={10}>
-            {Object.keys(props.data.genderData).length && (
-              <PieGraphView heading="Gender" data={sortObjectByKey(props.data.genderData)} />
-            )}
-          </GridItem>
-          <GridItem>
-            {Object.keys(props.data.dietaryRestrictionsData).length && (
-              <TableView
-                heading="Dietary Restrictions"
-                data={sortObjectByValue(props.data.dietaryRestrictionsData)}
-              />
-            )}
-          </GridItem>
-          <GridItem>
-            {Object.keys(props.data.trackPreferenceData).length && (
-              <PieGraphView
-                heading="Track Preference"
-                data={sortObjectByKey(props.data.trackPreferenceData)}
-              />
-            )}
-          </GridItem>
+            {Object.keys(props.data.schoolData).length ? (
+              <GridItem pt={10}>
+                <TableView heading="Universities" data={sortObjectByValue(props.data.schoolData)} />
+              </GridItem>) : null}
+            {Object.keys(props.data.majorData).length ? (
+              <GridItem pt={10}>
+                <TableView heading="Majors" data={sortObjectByValue(props.data.majorData)} />
+              </GridItem>) : null}
+            {Object.keys(props.data.schoolYearData).length ? (
+              <GridItem pt={10}>
+                <BarGraphView
+                  heading="School Year"
+                  data={sortObjectByKey(props.data.schoolYearData)}
+                />
+              </GridItem>) : null}
+            {Object.keys(props.data.shirtSizeData).length ? (
+              <GridItem pt={10}>
+                <TableView
+                  heading="Shirt Size"
+                  data={sortObjectByValue(props.data.shirtSizeData)}
+                />
+              </GridItem>) : null}
+            {Object.keys(props.data.dietaryRestrictionsData).length ? (
+              <GridItem pt={10}>
+                <TableView
+                  heading="Dietary Restrictions"
+                  data={sortObjectByValue(props.data.dietaryRestrictionsData)}
+                />
+              </GridItem>) : null}
+            {Object.keys(props.data.trackPreferenceData).length ? (
+              <GridItem pt={10}>
+                <PieGraphView
+                  heading="Track Preference"
+                  data={sortObjectByKey(props.data.trackPreferenceData)}
+                />
+              </GridItem>) : null}
+            {Object.keys(props.data.marketingData).length ? (
+              <GridItem pt={10}>
+                <PieGraphView
+                  heading="Marketing Source"
+                  data={sortObjectByKey(props.data.marketingData)}
+                />
+              </GridItem>) : null}
+            {Object.keys(props.data.genderData).length ? (
+              <GridItem pt={10}>
+                <PieGraphView heading="Gender" data={sortObjectByKey(props.data.genderData)} />
+              </GridItem>) : null}
         </Grid>}
       </AccordionPanel>
     </AccordionItem>

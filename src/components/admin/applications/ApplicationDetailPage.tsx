60b32ed769f14818c635dad2885c0f4fdb56ef34
ganning127 <ganningxu@gmail.com>
2025-02-21 02:16:30 -0500

    add support for checking in

--- src/components/admin/applications/ApplicationDetailPage.tsx
+++ src/components/admin/applications/ApplicationDetailPage.tsx
@@ -38,6 +38,7 @@ import {
   PopoverFooter,
   PopoverTrigger,
   Tooltip,
+  Spinner,
 } from "@chakra-ui/react";
 import { ErrorScreen, LoadingScreen, apiUrl, Service, handleAxiosError } from "@hex-labs/core";
 import axios from "axios";
@@ -78,6 +79,8 @@ const ApplicationDetailPage: React.FC = () => {
   const extendedDeadlines = watch("extendedDeadlines.enabled");
   const status = watch("status");
   const navigate = useNavigate();
+  const [checkInLoading, setCheckInLoading] = React.useState(false); // TODO: remove after hackgteeny
+  const [isCheckedIn, setIsCheckedIn] = React.useState(false); // TODO: remove after hackgteeny
 
   const addHttpsIfNeed = (url: string) => {
     if (!url.startsWith("https://") && !url.startsWith("http://")) {
@@ -189,6 +192,52 @@ const ApplicationDetailPage: React.FC = () => {
           </Box>
           <Box>
             <Flex gap="4px" flexDirection={{ base: "column", md: "row" }}>
+              {/* TEMP FOR HACKGTEENY (not using app) */}
+              <Button
+                colorScheme={
+                  isCheckedIn ? "gray" : "green"
+                }
+                size="sm"
+                onClick={async () => {
+                  try {
+                    setCheckInLoading(true);
+                    const body = {
+                      userId: data.userId,
+                      hexathon: data.hexathon,
+                      type: "check-in",
+                      identifier: "check-in",
+                    }
+  
+                    const res = await axios.post(apiUrl(Service.HEXATHONS, "/interactions"), body);
+
+                    if (res.status === 200) {
+                      toast({
+                        title: "Success",
+                        description: `${data.name} checked in successfully!`,
+                        status: "success",
+                        duration: 5000,
+                        isClosable: true,
+                      });
+                      setIsCheckedIn(true);
+                    }
+                  } catch (e: any) {
+                    handleAxiosError(e);
+                  }
+                  
+                  setCheckInLoading(false);
+                }}
+                disabled={isCheckedIn}
+              >
+                {
+                  checkInLoading && (
+                    <Spinner size="xs" mr={2} />
+                  )
+                }
+                {
+                  isCheckedIn ? "✅ Checked In" : "Check In"
+                }
+              </Button>
+
               <Button onClick={onOpen} size="sm" colorScheme="blue" alignSelf="start">
                 Edit Applicant Settings
               </Button>
@@ -197,7 +246,6 @@ const ApplicationDetailPage: React.FC = () => {
                   <Button
                     size="sm"
                     colorScheme="blue"
-                    alignSelf="start"
                     disabled={data.status !== "CONFIRMED"}
                   >
                     View Check-In QR Code
